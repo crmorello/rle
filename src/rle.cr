66c4114e5f0a45ff00999f49702c2b5a8d3d8b76
Corey Morello <cmorello@weatherflow.com>
2024-12-03 12:54:58 -0800

    Changed it to not distinguish the value and RLE everything.

--- src/rle.cr
+++ src/rle.cr
@@ -1,61 +1,38 @@
-# Simple RLE module that is meant to handle data with large swaths of 0s
 module RLE
   VERSION = "0.1.0"
-
+  
   def self.compress(data : Bytes) : Bytes
     buffer = IO::Memory.new(data.size)
     i = 0
-    
     while i < data.size
-      if data[i] == 0
-        count = count_zeros(data, i)
-        buffer.write_byte(0_u8)
-        buffer.write_bytes(count.to_u16, IO::ByteFormat::LittleEndian)
-        i += count
-      else
-        length = count_nonzeros(data, i)
-        buffer.write_byte(length.to_u8)
-        buffer.write(data[i, length])
-        i += length
-      end
+      value = data[i]
+      count = count_repeating(data, i)
+      
+      buffer.write_byte(value)
+      buffer.write_bytes(count.to_u16, IO::ByteFormat::LittleEndian)
+      i += count
     end
-    
     buffer.to_slice
   end
 
   def self.decompress(compressed : Bytes, original_size : Int64) : Bytes
     buffer = IO::Memory.new(original_size)
     i = 0
-    
     while i < compressed.size
-      length = compressed[i]
+      value = compressed[i]
       i += 1
+      count = IO::ByteFormat::LittleEndian.decode(UInt16, compressed[i, 2])
+      i += 2
       
-      if length == 0
-        zero_count = IO::ByteFormat::LittleEndian.decode(UInt16, compressed[i, 2])
-        buffer.write(Bytes.new(zero_count.to_i32, 0_u8))
-        i += 2
-      else
-        buffer.write(compressed[i, length])
-        i += length
-      end
+      buffer.write(Bytes.new(count.to_i32, value))
     end
-    
     buffer.to_slice
   end
 
-  private def self.count_zeros(data : Bytes, start : Int32) : Int32
+  private def self.count_repeating(data : Bytes, start : Int32) : Int32
     count = 0
-    while start < data.size && data[start] == 0 && count < UInt16::MAX
-      count += 1
-      start += 1
-    end
-    count
-  end
-
-  private def self.count_nonzeros(data : Bytes, start : Int32) : Int32
-    count = 0
-    while start < data.size && data[start] != 0 && count < 255
+    value = data[start]
+    while start < data.size && data[start] == value && count < UInt16::MAX
       count += 1
       start += 1
     end
